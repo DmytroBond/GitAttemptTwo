a50c2849960d0d9d102f1591af753e51fa04fcd8
DmytroBond <bond_dv@ukr.net>
2023-07-18 00:17:27 +0300

    com 002 17.07.23

--- src/GitAttemptTwo/GitAttemptTwo.java
+++ src/GitAttemptTwo/GitAttemptTwo.java
@@ -5,6 +5,8 @@ package GitAttemptTwo;
 		public static void main(String[] args) {
 			Point2D p = new Point2D(0.0, 0.0);
 			System.out.println("p = " + p);
+			System.out.println("p.X = " + p.getX());
+			System.out.println("p.Y = " + p.getY());
 		}
 
 	}
